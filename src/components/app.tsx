897f88129f688164221aae26dd3184be277e4a9f
Sergii Mykhailov <sergii.mykhailov@gmail.com>
2020-11-25 17:35:21 +0100

    Added feed retreiving logic

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import Feed from './feed';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <div>Ostapoff Basic CRA Template</div>
       <Feed />
-    </>
+    </ApolloProvider>
   );
 };
 
--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,11 +1,27 @@
-import { useQuery } from '@apollo/client';
-import { useFeedQuery } from 'data/generated/graphql';
 import React from 'react';
+import { useFeedQuery } from 'data/generated/graphql';
 
 const Feed = () => {
   const { data, loading, error } = useFeedQuery();
 
-  return <div>Feed goes here</div>;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error {error}</div>;
+  }
+
+  return (
+    <>
+      <div>Feed goes here</div>
+      <ul>
+        {data?.feed?.map((item) => {
+          return <li></li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default Feed;
