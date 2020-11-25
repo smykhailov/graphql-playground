5d018afe35d70f6babfc4484d8699523c5d7d918
Sergii Mykhailov <sergii.mykhailov@gmail.com>
2020-11-25 20:22:38 +0100

    Added resolvers

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,23 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Feed from './feed';
 
+const resolvers = {
+  Query: {
+    feed: () => {
+      return [
+        {
+          id: '1',
+          title: 'First Feed',
+          description: 'some feed',
+        },
+      ];
+    },
+  },
+};
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
+  resolvers: resolvers,
 });
 
 const App = () => {
